b9872c73e062e06cf63fdda123f3fbc52e177eaf
Saha Lopatko <lopakoaleksandr@gmail.com>
2024-11-11 15:52:30 +0200

    add game CheckIQ in page MiniGame

--- src/pages/GamePage/ui/GamePage.tsx
+++ src/pages/GamePage/ui/GamePage.tsx
@@ -2,13 +2,13 @@ import { FC } from "react";
 
 import style from "./GamePage.module.scss";
 import { ColorSwap } from "shared/components/MiniGames/ColorSwap/ColorSwap";
-import { GuessNumber } from "shared/components/MiniGames/GuessNumber/GuessNumber";
+import { CheckIQ } from "shared/components/MiniGames/GuessNumber/CheckIQ";
 import { FindCircule } from "shared/components/MiniGames/FindCircule/FindCircule";
 import { useAppSelector } from "app/store/useAppSelector";
 
 export const nameGames: Record<string, FC> = {
   ColorSwap: ColorSwap,
-  GuessNumber: GuessNumber,
+  CheckIQ: CheckIQ,
   FindCircule: FindCircule,
 };
 
